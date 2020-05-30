b78f361c67ac438baebd256cd218fe45f950cb71
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-05-30 15:28:53 +0530

    Starts various instances of same service

--- Unzip_service_starter.sh
+++ Unzip_service_starter.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SUBSTR2='.tar'
 starter_var1=$(tail -1 /var/local/Unzipper_log_file.txt)
-starter_var2=${starter_var1//$".zip"/}
-sudo systemctl start Unzipper@"$starter_var2".service
+starter_name_only="${starter_var1%%.*}"                  # To remove all the string part after first dot.
+
+
+sudo systemctl start Unzipper@"$starter_name_only".service
