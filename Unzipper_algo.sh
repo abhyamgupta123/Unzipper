3b07111e987d26e5721655ba59c4f001c6afd8cd
abhyamgupta123 <abhyamgupta123@gmail.com>
2020-05-30 15:28:20 +0530

    Contains all the process of doing task

--- Unzipper_algo.sh
+++ Unzipper_algo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 unzip_var1=$(tail -1 /var/local/Unzipper_log_file.txt)
-unzip_var2=${unzip_var1//$".zip"/}
+unzip_name_only="${unzip_var1%%.*}"      # To remove all the string part after first dot.
+unzip_var2=${unzip_var1//$".zip"/}       # To remove .zip extention from the string name
 killall notify-osd
 notify-send "UNZIPPER" "$unzip_var1 is created in Downloads"
 sleep 0.3
@@ -33,20 +34,22 @@ if grep -q "$SUBSTR1" <<< "$unzip_var1" ; then
     fi
   done
 elif grep -q "$SUBSTR2" <<< "$unzip_var1" ; then
+  unzip_last_extention="${unzip_var1##*.}"            # To obtain last extention part of file string
+  unzip_var3=${unzip_var1//$".tar.$unzip_last_extention"/}          # To obtain the name after removing .tar.zomething from string name
   killall notify-osd
   notify-send "UNZIPPER" "Waiting for the tar file to be downloaded completely."
   sleep 0.2
 
   while true ; do
-    if [ -f "/home/USER/Downloads/$unzip_var1.part" ];then
+    if [ -f "/home/USER/Downloads/$unzip_var1.part" ] ; then
       continue
     fi
     if [ -e /home/USER/Downloads/"$unzip_var1" ] ; then
       killall notify-osd
       notify-send "UNZIPPER" "Unzipping $unzip_var1"
-      mkdir /home/USER/Downloads/"$unzip_var2"
-      tar -xf /home/USER/Downloads/"$unzip_var1" -C /home/USER/Downloads/"$unzip_var2"
-      chmod -R 777 /home/USER/Downloads/"$unzip_var2"
+      mkdir /home/USER/Downloads/"$unzip_var3"
+      tar -xf /home/USER/Downloads/"$unzip_var1" -C /home/USER/Downloads/"$unzip_var3"
+      chmod -R 777 /home/USER/Downloads/"$unzip_var3"
       rm /home/USER/Downloads/"$unzip_var1"
       killall notify-osd
       notify-send "UNZIPPER" "$unzip_var1 is extracted successfully..!!"
@@ -59,4 +62,4 @@ elif grep -q "$SUBSTR2" <<< "$unzip_var1" ; then
   done
 fi
 
-sudo systemctl stop Unzipper@"$unzip_var2".service
+sudo systemctl stop Unzipper@"$unzip_name_only".service
